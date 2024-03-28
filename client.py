2a887e42e1cf78b255f21f7265b9ea463afb0387
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-03-27 18:56:19 -0700

    Update

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 from socket import *
 
-if __name__ == '__main__':
+def displayTransactions(transactions):
+    pass
+
+
+def main():
     serverName = 'localhost'
     serverPort = 12000
 
@@ -8,8 +12,46 @@ if __name__ == '__main__':
     clientSocket = socket(AF_INET, SOCK_DGRAM)
 
     #getting keyboard input from the client.
-    username = input("Enter username: ")
-    password = input("Enter password: ")
+    username = input('Enter username: ')
+    password = input('Enter password: ')
 
-    clientSocket.sendto(username.encode(), serverName, serverPort)
-    clientSocket.sendto(password.encode(), serverName, serverPort)
+    #created hashmap/dictionary to store username and password.
+    data = {'username': username, 'password': password}
+    
+    #sending username and password to the server.
+    clientSocket.sendto(data.encode(), serverName, serverPort)
+
+    #receiving response and address from the server.
+    response, serverAddress = clientSocket.recvfrom(2048)
+    decodedResponse = response.decode()
+
+    if decodedResponse['authenticated']:
+        print(f'User {username} is authenicated.')
+        print(f'Balance: {decodedResponse['balance']}')
+
+        #storing the balance and the transactions
+        balance = decodedResponse['balance']
+        transactions = decodedResponse['txs']
+
+        displayTransactions(transactions)
+
+    else: #server rejects the message.
+        print(f'Authenication failed for user {username}.')
+        choose = input('Choose one of the following options?\n'
+                       'a. Enter the username and password again\n'
+                       'b. Quit the program.\n'
+                       'Enter your choice: ')
+        
+        #close the socket and terminate the client program.
+        if choose.lower() == 'b':
+            clientSocket.close()
+            exit()
+
+        else: #recall main again, so the user can enter their username and password.
+            main()
+
+    #closing the socket.
+    clientSocket.close()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -2,14 +2,32 @@ from socket import *
 
 if __name__ == '__main__':
     serverPort = 12000 
+
     #creating a UDP socket for the server.
     serverSocket = socket(AF_INET, SOCK_DGRAM) 
+
+    #bind socket to the port.
     serverSocket.bind(serverPort)
-    print("The server is ready to receive.")
+
+    print('The server is ready to receive.')
 
     clientList = ['A', 'B', 'C', 'D']
     transactionsList = []
 
+    users = { #created hashmap/dictionary to store all possible user information.
+        'A': {'password': 'A', 'balance': 10, 'txts': []},
+        'B': {'password': 'B', 'balance': 10, 'txts': []},
+        'C': {'password': 'C', 'balance': 10, 'txts': []},
+        'D': {'password': 'D', 'balance': 10, 'txts': []}
+    }
+
     while True: #run the server infinitely.
         #read from socket into message and get the clients address.
         message, clientAddress = serverSocket.recvfrom(2048)
+        decodedMessage = message.decode()
+
+        #receiving username and password from the client.
+        username = decodedMessage['username']
+        password = decodedMessage['password']
+
+        response = {}
