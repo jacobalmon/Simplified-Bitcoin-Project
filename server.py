94c569e3f0eea6a894d203c3977f5c1dc950c4b3
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-03-29 15:56:27 -0700

    Update

--- server.py
+++ server.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
 
     #created hashmap/dictionary to store all possible user information.
     users = { #Note the user's username is used as the key.
-        'A': {'password': 'A', 'balance': 10, 'txts': []},
-        'B': {'password': 'B', 'balance': 10, 'txts': []},
-        'C': {'password': 'C', 'balance': 10, 'txts': []},
-        'D': {'password': 'D', 'balance': 10, 'txts': []}
+        'A': {'password': 'A', 'balance': 10, 'txs': [], 'tx_id': 100},
+        'B': {'password': 'B', 'balance': 10, 'txs': [], 'tx_id': 200},
+        'C': {'password': 'C', 'balance': 10, 'txs': [], 'tx_id': 300},
+        'D': {'password': 'D', 'balance': 10, 'txs': [], 'tx_id': 400}
     }
 
     while True: #run the server infinitely.
@@ -38,7 +38,8 @@ if __name__ == '__main__':
         if username in users and users[username]['password'] == password:
             response['authenticated'] = True
             response['balance'] = users[username]['balance']
-            response['txs'] = users[username]['txts']
+            response['txs'] = users[username]['txs']
+            response['tx_id'] = users[username]['tx_id']
 
         else:
             response['authenticated'] = False
